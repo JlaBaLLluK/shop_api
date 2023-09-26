a4a105b84419bb351c7d687927ea344cac72afc5
Egor <etkachev398@gmail.com>
2023-09-26 11:29:27 +0300

    Added secret key to env

--- shop_project/shop_project/shop_project/settings.py
+++ shop_project/shop_project/shop_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import environ
 
+env = environ.Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ezc@@*+wr_bcknvr29)!=7^v^8t++%-94sm=#-2i78wbn2%+(t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,9 +74,6 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-env = environ.Env()
-environ.Env.read_env()
 
 DATABASES = {
     'default': {
