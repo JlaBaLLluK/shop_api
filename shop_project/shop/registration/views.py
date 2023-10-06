c2893069f30cd04a26b741dc6f83aa2ec730fb15
Egor <etkachev398@gmail.com>
2023-10-06 20:42:59 +0300

    Added password validation

--- shop_project/shop/registration/views.py
+++ shop_project/shop/registration/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.status import *
 from rest_framework.views import APIView
@@ -14,10 +16,15 @@ class UserRegistrationView(APIView):
         if not serializer.is_valid():
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+        try:
+            validate_password(request.data.get('password'))
+        except ValidationError as error:
+            return Response({'error': error.messages}, status=HTTP_400_BAD_REQUEST)
+
         user = AuthUser.objects.create_user(username=request.data.get('username'),
                                             password=request.data.get('password'),
                                             email=request.data.get('email'),
                                             first_name=request.data.get('first_name'),
                                             last_name=request.data.get('last_name'),
                                             )
-        return Response(UserRegistrationSerializer(user).data, status=HTTP_201_CREATED)
+        return Response(UserRegistrationSerializer(user.data), status=HTTP_201_CREATED)
