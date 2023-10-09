3f5579a0def6ed8e2ac87225ed76bd1b39a5aec3
Egor <etkachev398@gmail.com>
2023-10-09 19:55:43 +0300

    Added 'is_new' field for advertisement model

--- shop_project/shop/sale_advertisement/serializers.py
+++ shop_project/shop/sale_advertisement/serializers.py
@@ -6,7 +6,8 @@ from sale_advertisement.models import SaleAdvertisement
 class AllAdvertisementsSerializer(ModelSerializer):
     class Meta:
         model = SaleAdvertisement
-        fields = ('advertisement_title', 'publish_date', 'advertisement_price', 'currency', 'advertisement_location')
+        fields = ('advertisement_title', 'publish_date',
+                  'advertisement_price', 'currency', 'advertisement_location', 'is_new')
 
 
 class SingleAdvertisementSerializer(ModelSerializer):
