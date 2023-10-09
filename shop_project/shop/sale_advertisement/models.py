5b415bd5b055be445480b78ff9f0e0a908fd5a20
Egor <etkachev398@gmail.com>
2023-10-09 18:03:50 +0300

    Added SaleAdvertisement model

--- shop_project/shop/sale_advertisement/models.py
+++ shop_project/shop/sale_advertisement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from user.models import AuthUser
+
+
+class SaleAdvertisement(models.Model):
+    advertisement_title = models.CharField(max_length=150, blank=False)
+    advertisement_description = models.TextField(blank=False)
+    advertisement_location = models.CharField(max_length=150, blank=False)
+    publish_date = models.DateTimeField(auto_now=True)
+    advertisement_price = models.DecimalField(max_digits=10, decimal_places=2)
+    CURRENCIES = [
+        ('BYN', 'Belorussian rubles'),
+        ('RUB', 'Russian rubles'),
+        ('USD', 'American dollars'),
+        ('EUR', 'Euro')
+    ]
+    currency = models.CharField(max_length=3, blank=False, choices=CURRENCIES)
+    advertisement_author = models.ForeignKey(AuthUser, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Advertisements'
