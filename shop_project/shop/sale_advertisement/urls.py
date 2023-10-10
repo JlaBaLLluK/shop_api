d6ec60f2a9b0bc139a55113d53c22d74934fb46f
Egor <etkachev398@gmail.com>
2023-10-10 11:52:25 +0300

    Added the ability to create, change, get adn delete single advertisement

--- shop_project/shop/sale_advertisement/urls.py
+++ shop_project/shop/sale_advertisement/urls.py
@@ -3,4 +3,6 @@ from sale_advertisement.views import *
 
 urlpatterns = [
     path('<int:page_number>/', AllAdvertisementsView.as_view()),
+    path('create-advertisement/', CreateAdvertisementView.as_view()),
+    path('<int:page_number>/<int:pk>/', SingleAdvertisementView.as_view()),
 ]
--- shop_project/shop/sale_advertisement/views.py
+++ shop_project/shop/sale_advertisement/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.status import *
 from sale_advertisement.serializers import *
@@ -19,9 +21,54 @@ class AllAdvertisementsView(APIView):
         return Response(serializer.data, status=HTTP_200_OK)
 
 
+class CreateAdvertisementView(APIView):
+    serializer_class = SingleAdvertisementSerializer
+
+    @staticmethod
+    def post(request):
+        serializer = SingleAdvertisementSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+        serializer.create(serializer.validated_data)
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+
 class SingleAdvertisementView(APIView):
     serializer_class = SingleAdvertisementSerializer
 
     @staticmethod
-    def get(request, page_number, pk):
-        pass
+    def get_advertisement(pk):
+        try:
+            advertisement = get_object_or_404(SaleAdvertisement, pk=pk)
+        except Http404:
+            return None
+
+        return advertisement
+
+    def get(self, request, page_number, pk):
+        advertisement = self.get_advertisement(pk)
+        if advertisement is None:
+            return Response({"errors": "This advertisement doesn't exist!"}, status=HTTP_404_NOT_FOUND)
+
+        return Response(SingleAdvertisementSerializer(advertisement).data, status=HTTP_200_OK)
+
+    def put(self, request, page_number, pk):
+        advertisement = self.get_advertisement(pk)
+        if advertisement is None:
+            return Response({"errors": "This advertisement doesn't exist!"}, status=HTTP_404_NOT_FOUND)
+
+        serializer = SingleAdvertisementSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+        serializer.update(advertisement, serializer.validated_data)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def delete(self, request, page_number, pk):
+        advertisement = self.get_advertisement(pk)
+        if advertisement is None:
+            return Response({"errors": "This advertisement doesn't exist!"}, status=HTTP_404_NOT_FOUND)
+
+        advertisement.delete()
+        return Response({"success": "Advertisement was deleted successfully!"}, status=HTTP_200_OK)
