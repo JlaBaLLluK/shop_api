b401559bb44183f5770bfe98ec60c444d56dcc63
Egor <etkachev398@gmail.com>
2023-10-11 21:57:11 +0300

    Added sorting advertisements by publish date and popularity

--- shop_project/shop/sale_advertisement/serializers.py
+++ shop_project/shop/sale_advertisement/serializers.py
@@ -6,8 +6,8 @@ from sale_advertisement.models import SaleAdvertisement
 class AllAdvertisementsSerializer(ModelSerializer):
     class Meta:
         model = SaleAdvertisement
-        fields = ('advertisement_title', 'publish_date',
-                  'advertisement_price', 'currency', 'advertisement_location', 'is_new')
+        fields = ('advertisement_title', 'advertisement_price', 'currency', 'publish_date',
+                  'advertisement_location', 'is_new', 'views_amount')
 
 
 class SingleAdvertisementSerializer(ModelSerializer):
--- shop_project/shop/sale_advertisement/views.py
+++ shop_project/shop/sale_advertisement/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import BadRequest
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
@@ -11,13 +12,35 @@ class AllAdvertisementsView(APIView):
     serializer_class = AllAdvertisementsSerializer
     MAX_ADVERTISEMENTS_ON_PAGE = 20
 
+    def get_sorted_advertisements(self, page_number, sort_order):
+        advertisements = SaleAdvertisement.objects.all()
+        slice_lower_bound = (page_number - 1) * self.MAX_ADVERTISEMENTS_ON_PAGE
+        slice_upper_bound = self.MAX_ADVERTISEMENTS_ON_PAGE * page_number
+        if sort_order == 'newest':
+            advertisements = advertisements.order_by('-publish_date').values()
+        elif sort_order == 'oldest':
+            advertisements = advertisements.order_by('publish_date').values()
+        elif sort_order == 'most_popular':
+            advertisements = advertisements.order_by('-views_amount').values()
+        elif sort_order == 'most_unpopular':
+            advertisements = advertisements.order_by('views_amount').values()
+        elif sort_order is None:
+            pass
+        else:
+            raise BadRequest
+
+        return advertisements[slice_lower_bound:slice_upper_bound]
+
     def get(self, request, page_number):
-        goods = SaleAdvertisement.objects.all()[
-                (page_number - 1) * self.MAX_ADVERTISEMENTS_ON_PAGE: self.MAX_ADVERTISEMENTS_ON_PAGE * page_number]
-        if len(goods) == 0:
+        sort_order = request.query_params.get('sort_order')
+        try:
+            advertisements = self.get_sorted_advertisements(page_number, sort_order)
+        except BadRequest:
+            return Response({"error": "Unknown sort order!"}, status=HTTP_400_BAD_REQUEST)
+        if len(advertisements) == 0:
             return Response({"information": "There is no data yet!"}, status=HTTP_204_NO_CONTENT)
 
-        serializer = AllAdvertisementsSerializer(goods, many=True)
+        serializer = AllAdvertisementsSerializer(advertisements, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
 
