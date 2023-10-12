e1fd5b3219fb1b10f428b8cce87dced13ffc1b1b
Egor <etkachev398@gmail.com>
2023-10-12 13:42:15 +0300

    Removed currency column from advertisement

--- shop_project/shop/sale_advertisement/serializers.py
+++ shop_project/shop/sale_advertisement/serializers.py
@@ -6,7 +6,7 @@ from sale_advertisement.models import SaleAdvertisement
 class AllAdvertisementsSerializer(ModelSerializer):
     class Meta:
         model = SaleAdvertisement
-        fields = ('advertisement_title', 'advertisement_price', 'currency', 'publish_date',
+        fields = ('advertisement_title', 'advertisement_price', 'publish_date',
                   'advertisement_location', 'is_new', 'views_amount')
 
 
