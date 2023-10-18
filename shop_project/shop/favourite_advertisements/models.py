673785698ae4eb6571e42c056aaa125e04c57f43
Egor <etkachev398@gmail.com>
2023-10-18 13:36:16 +0300

    Removed advertisements amount column from favourite advertisements table

--- shop_project/shop/favourite_advertisements/models.py
+++ shop_project/shop/favourite_advertisements/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
-
 from sale_advertisement.models import SaleAdvertisement
 from user.models import AuthUser
 
@@ -8,7 +6,6 @@ from user.models import AuthUser
 class FavouriteAdvertisements(models.Model):
     user = models.OneToOneField(AuthUser, on_delete=models.CASCADE)
     advertisements = models.ForeignKey(SaleAdvertisement, on_delete=models.CASCADE)
-    advertisements_amount = models.PositiveIntegerField(default=0)
     adding_date = models.DateTimeField()
 
     class Meta:
