045c5ae8bc14cb786da90a5c949d85fd1769e4ca
Egor <etkachev398@gmail.com>
2023-10-24 12:01:00 +0300

    Added username validation for spaces

--- shop_project/shop/user/models.py
+++ shop_project/shop/user/models.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from services.validators import validate_username
 
 
 class AuthUser(AbstractUser):
+    username = models.CharField(max_length=50, blank=False, unique=True, validators=[validate_username])
+
     class Meta:
         db_table = 'Users'
